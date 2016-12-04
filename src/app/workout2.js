08c5c7ac167f18a66623f0827a4dae4a9a3fc6ed
iamchenxin <iamchenxin@gmail.com>
2016-12-04 07:32:08 -0700

    ts

--- src/app/workout2.js
+++ src/app/workout2.js
@@ -6,12 +6,12 @@ import type { WxTextMsg } from '../wxlib/wxxml_compiler.js';
 
 async function workout2(ctx: Context, next: () => Promise<void>): Promise<void> {
   const wxres = await makeWxRes(ctx);
+  const delay = Math.floor(Math.random()*3000);
   let msg: WxTextMsg = {
-    Content: ` hello ${wxres.msgIn.FromUserName}`,
+    Content: ` hello ${wxres.msgIn.FromUserName}, will send again(${delay})`,
     MsgType: 'text',
   };
   wxres.send(msg);
-  const delay = Math.floor(Math.random()*3000);
   setTimeout(() => {
     msg.Content = ' iam send again~';
     wxres.send(msg);
